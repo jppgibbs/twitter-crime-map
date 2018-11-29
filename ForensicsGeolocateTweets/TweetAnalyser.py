42938226a94e5e59381b197ec0b8f87b8cfc7f8e
Joe Gibbs <jppgibbs@gmail.com>
2018-11-29 21:49:32 +0000

    a

--- ForensicsGeolocateTweets/TweetAnalyser.py
+++ ForensicsGeolocateTweets/TweetAnalyser.py
@@ -8,9 +8,10 @@ import webbrowser
 
 # Window
 win = tk.Tk()
-win.title("Forensic Tweet Analyser")
+win.title("Forensic Tweet Analyzer")
 win.minsize(width=900, height=500)
 win.maxsize(width=900, height=500)
+win.configure(background="#eaeaea")
 win.resizable(0,0)
 
 
@@ -42,8 +43,8 @@ def SearchJSON():
                 print("Nothing Found")    
         map_osm.save('plotted.html')
         
-def SearchTemplate():
-    beerKeywords = ["beer", "Beer", "lager", "Lager", "Stout", "stout", "Pilsner", "pilsner", "Ale", "ale", "Stout", "stout", "mead", "Mead", "porter", "Porter", "Trappist", "trappist"]
+def SearchBeerTemplate():
+    beerKeywords = ["beer", "lager", "stout", "pilsner", "ale", "stout", "mead", "porter", "trappist"]
     map_osm = folium.Map(location=[53.472328361821766,-2.23959064483645])
     with open('tweets.json') as data_file:
         countt = 0
@@ -52,12 +53,14 @@ def SearchTemplate():
             tempText = data['text']
             latt = data['geoLocation']['latitude']
             longg = data['geoLocation']['longitude']
-        for word in beerKeywords:
-            if word in tempText:
-                countt = countt+1
-                StringToScroll = "\n\n["+str(countt)+"] Date: " + data['createdAt']['$date'] + "Tweet Text:" + data['text']
-                folium.Marker([latt,longg], popup=tempText).add_to(map_osm)
-                scr.insert(tk.INSERT,StringToScroll)
+            for word in beerKeywords:
+                if word in tempText:
+                    countt = countt + 1
+                    StringToScroll = "\n\n["+str(countt)+"] Date: " + data['createdAt']['$date'] + " Tweet Text:" + data['text']
+                    folium.Marker([latt,longg], popup="").add_to(map_osm)
+                    scr.insert(tk.INSERT,StringToScroll)
+        map_osm.save('plotted.html')
+
     
 def ShowPlottedSearchResults():
     webbrowser.open_new_tab('plotted.html')
@@ -83,7 +86,7 @@ ttk.Label(win, text="Search String:").place(x=50, y=50)
 
 # Adding a Textbox Entry widget
 name = tk.StringVar()
-nameEntered = ttk.Entry(win, width=30, textvariable=name)
+nameEntered = ttk.Entry(win, width=30, textvariable=name,)
 nameEntered.place(x=5, y=70)
 
 # Adding a Button
@@ -93,27 +96,26 @@ action.place(x=5, y=93)
 # Using a scrolled Text control
 scrolW = 84
 scrolH = 30
-scr = scrolledtext.ScrolledText(win, width=scrolW, height=scrolH, wrap=tk.WORD)
+scr = scrolledtext.ScrolledText(win, width=scrolW, height=scrolH, wrap=tk.WORD, background="#f0f0f0")
 scr.place(x=200, y=1)
+
+# Search Label Beer
+ttk.Label(win, text="Search Template:").place(x=40, y=140)
+
+# Adding a Button
+action = ttk.Button(win, width=30, text="Beers", command=SearchBeerTemplate)
+action.place(x=5, y=160)
 
 # Adding a Button
 loadingJsonButton = ttk.Button(win, width=30, text=" Show Plotted Search Results ", command=ShowPlottedSearchResults)
 loadingJsonButton.place(x=5, y=250)
 
-# Search Label
-ttk.Label(win, text="Search Template:").place(x=40, y=140)
-
-
 # Combobox
-combo = Combobox(win, width=28)
-combo['values']= ("Beer", 2, 3, 4, 5, "Text")
-combo.current(0) #set the selected item
-combo.place(x=5, y=160)
+#combo = Combobox(win, width=28)
+#combo['values']= ("Beer", 2, 3, 4, 5, "Text")
+#combo.current(0) #set the selected item
+#combo.place(x=5, y=160)
 #combo.get()
-
-# Adding a Button
-action = ttk.Button(win, width=30, text="Search!", command=SearchJSON)
-action.place(x=5, y=182)
 
 # Place cursor into name Entry
 nameEntered.focus()
